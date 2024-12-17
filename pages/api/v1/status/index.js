2181f74b73cca507235aafe8adb442668a2d5717
Daniel Lopes <daniellopesdasilva98@gmail.com>
2024-12-17 15:01:04 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,13 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  // console.log("database = ", database);
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+
   response
     .status(200)
     .json({ chave: "Resultado da chamada da API! Funciona acentos ? áéàèãâ" });
 }
 
 export default status;
- 
